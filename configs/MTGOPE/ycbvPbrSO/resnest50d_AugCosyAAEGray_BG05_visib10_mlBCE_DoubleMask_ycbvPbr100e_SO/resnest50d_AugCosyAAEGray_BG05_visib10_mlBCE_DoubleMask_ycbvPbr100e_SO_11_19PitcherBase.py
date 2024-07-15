5179f6f4c2619eb181761438fc1baf6c9a1f2861
hoangcuongbk80 <hoangcuongbk80@gmail.com>
2024-07-15 16:46:21 +0700

    initial commit

--- configs/MTGOPE/ycbvPbrSO/resnest50d_AugCosyAAEGray_BG05_visib10_mlBCE_DoubleMask_ycbvPbr100e_SO/resnest50d_AugCosyAAEGray_BG05_visib10_mlBCE_DoubleMask_ycbvPbr100e_SO_11_19PitcherBase.py
+++
@@ -1,5 +0,0 @@
-_base_ = "./resnest50d_AugCosyAAEGray_BG05_visib10_mlBCE_DoubleMask_ycbvPbr100e_SO_01_02MasterChefCan.py"
-OUTPUT_DIR = (
-    "output/DGNPose/ycbvPbrSO/resnest50d_AugCosyAAEGray_BG05_visib10_mlBCE_DoubleMask_ycbvPbr100e_SO/11_19PitcherBase"
-)
-DATASETS = dict(TRAIN=("ycbv_019_pitcher_base_train_pbr",))
--- configs/MTGOPE/ycbvPbrSO/resnest50d_AugCosyAAEGray_BG05_visib10_mlBCE_DoubleMask_ycbvPbr100e_SO_bop_test/resnest50d_AugCosyAAEGray_BG05_visib10_mlBCE_DoubleMask_ycbvPbr100e_SO_bop_test_02_03CrackerBox.py
+++
@@ -1,5 +0,0 @@
-_base_ = "./resnest50d_AugCosyAAEGray_BG05_visib10_mlBCE_DoubleMask_ycbvPbr100e_SO_bop_test_01_02MasterChefCan.py"
-OUTPUT_DIR = (
-    "output/DGNPose/ycbvPbrSO/resnest50d_AugCosyAAEGray_BG05_visib10_mlBCE_DoubleMask_ycbvPbr100e_SO/02_03CrackerBox"
-)
-DATASETS = dict(TRAIN=("ycbv_003_cracker_box_train_pbr",))
